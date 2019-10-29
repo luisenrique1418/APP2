3086df4e9c71e33c8cea42d111b319d3729df723
luisenrique1418 <49424604+luisenrique1418@users.noreply.github.com>
2019-10-28 21:58:41 -0600

    1

--- APP2/Site.Master.designer.cs
+++ APP2/Site.Master.designer.cs
@@ -7,11 +7,40 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
-namespace APP2 {
-    
-    
-    public partial class SiteMaster {
-        
+namespace APP2
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
         /// <summary>
         /// Control MainContent.
         /// </summary>
